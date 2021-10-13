eec45d955ee0a2d5400ddb42c4173301338edde6
WIX3Y <wikstromdenice@gmail.com>
2021-10-13 21:11:57 +0200

    Update integer.cpp

--- MA4_files/integer.cpp
+++ MA4_files/integer.cpp
@@ -37,22 +37,7 @@ extern "C"{
 		}
 	}
 
-// fibonacci
-int fib(int n){
-    if (n <= 1)
-        return n;
-    return fib(n-1) + fib(n-2);
-}
-
-int main ()
-{
-    int n = 9;
-    cout << fib(n);
-    getchar();
-    return 0;
-}
-
-//Fibonacci Series using Recursion
+//Fibonacci
 int fib(int n){
     if (n <= 1)
         return n;
