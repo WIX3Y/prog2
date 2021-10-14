93f07cfdfb2bfc5f6109a369cad94ce0347fc716
WIX3Y <wikstromdenice@gmail.com>
2021-10-14 14:26:24 +0200

    Update integer.cpp

--- MA4_files/integer.cpp
+++ MA4_files/integer.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 // Integer class
 
 class Integer{
@@ -8,7 +8,7 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
-		int fib(int val);
+//		int fib(int val);
 	private:
 		int val;
 	};
@@ -17,14 +17,14 @@ Integer::Integer(int n){
 	val = n;
 	}
 
-int Integer::fib(int value){
+/*int Integer::fib(int value){
 	if((value==1)||(value==0)){
 		return(value);
 		}
 	else{
 		return(fib(value-1)+fib(value-2));
 		}
-	}
+	}*/
 
 int Integer::get(){
 	return val;
@@ -37,7 +37,7 @@ void Integer::set(int n){
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
-	int Integer_fib(Integer* integer, int n) {return integer->fib(get());}
+//	int Integer_fib(Integer* integer, int n) {return integer->fib(n);}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
